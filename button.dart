6ad67062c05cc7fcaaeaecd8b54ddfc5ab8e23f7
SquishyWubbles <48323673+SquishyWubbles@users.noreply.github.com>
2021-07-27 16:04:13 +0200

    button.dart
    whatever

---
+++ button.dart
@@ -0,0 +1,12 @@
+TextButton(
+    style: ButtonStyle(
+    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+       borderRadius: BorderRadius.circular(18.0),
+        side: BorderSide(color: Colors.red),
+        ),
+       ),
+      ),
+     onPressed: () {},
+     child: Text('what'),
+   ),
